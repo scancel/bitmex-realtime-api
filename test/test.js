93745b1661e163ca42879b01ccbace8ac4f57ea6
Itamar Burshtein <itamar@budsoft.co.il>
2020-06-15 17:31:25 +0300

    fixed multiple orders update issue

--- test/test.js
+++ test/test.js
@@ -1,11 +1,14 @@
 'use strict';
 var BitMEXClient = require('../index');
 
-var client = new BitMEXClient({testnet: true});
+var client = new BitMEXClient({apiKeyID: "YhwDIPnvtb_lumoW79xjoKjR", apiKeySecret: "2N6sj9eX4iYuwDqrisqU2slIXQ4TGQmitonS0l1YHaFlkakK"});
 
-/*client.addStream('XBTUSD', 'quote', function(data, symbol, table) {
-  console.log('Update on ' + table + ':' + symbol + '. New data:\n', data, '\n');
-});*/
+client.addStream('XBTUSD', 'order', function(data, symbol, table) {
+  if(data.length > 0){
+      const orders = data.map(o => ({ id: o.orderID, filled: o.cumQty, status: o.ordStatus }));
+      console.log('Update on ' + table + ':' + symbol + '. New data:\n', orders[orders.length - 1], '\n');
+  }
+});
 
 client.on('error', function(e) {
   console.error('Received error:', e);
